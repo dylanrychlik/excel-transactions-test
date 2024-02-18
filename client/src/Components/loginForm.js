7a27ae89e25f51db55c55a0307c5ed3a1a6ccf42
Dylan Rychlik <dylanrychlik@gmail.com>
2024-02-18 00:13:21 -0500

    test

--- client/src/Components/loginForm.js
+++ client/src/Components/loginForm.js
@@ -112,7 +112,7 @@ function Form(props) {
                   password: password
              };
     
-            axios.post("http://http-login-validation-rychlik.onrender.com/3001/login", user)
+            axios.post("https://http-login-validation-rychlik.onrender.com/login", user)
                 .then(function (response) {
                     console.log('Authentication data: ', response.data);
                     if (response.data === 'login successful') {
