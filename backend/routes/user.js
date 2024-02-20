7f3838eb79f81e7d1b2f47bd064bbebca696e873
Dylan Rychlik <dylanrychlik@gmail.com>
2024-02-19 23:33:03 -0500

    test

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -228,7 +228,7 @@ exports.reset = function (req, res) {
 
       var pass = post.confirmpassword;
 
-      var email = req.body.EMAIL;
+      var email = req.body.email;
       let userdata = {
          email: 'dylanrychlik@gmail.com',
 
